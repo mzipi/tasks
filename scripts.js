159710009223a9eb3bab2f3943189dfaa7b6eadf
Michell Zipitría <mzipi@outlook.com>
2021-04-06 20:10:33 -0300

    botones agregados

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 window.onload = function(){
     document.getElementById('agregar').addEventListener('click', function(){
         item = document.createElement('li');
-        del = document.createElement
-        texto = document.getElementById('caja').value;
-        item.append(texto);
+        del = document.createElement('button');
+        edit = document.createElement('button');
+        
+        del.append('Borrar');
+        edit.append('Editar');
+
+        item.append(document.getElementById('caja').value);
+        item.appendChild(del);
+        item.appendChild(edit);
+
         document.getElementById('tareas').appendChild(item);
     });
 }
